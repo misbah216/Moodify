b611debfa0c99c9a468163d57d35a94e603b3532
Misbah Saifi <saifimisbah7@gmail.com>
2026-08-12 13:39:24 +0530

    moodify-day3

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const blacklistModel = require("../models/blacklist.model")
 
 async function registerUser(req, res) {
     const {username, email, password} = req.body;
@@ -42,7 +43,7 @@ async function loginUser(req, res) {
             {email},
             {username}
         ]
-    });
+    }).select("+password");
 
     if(!user){
         return res.status(400).json({message: "Invalid credentials"});
@@ -73,4 +74,23 @@ async function loginUser(req, res) {
     })
 }
 
-module.exports = { registerUser, loginUser }
+async function getMe(req,res){
+    const user = await userModel.findById(req.user.id).select("-password");
+    res.status(200).json({
+        message: "User fetched successfully",
+        user
+    })
+}
+
+async function logoutUser(req,res){
+    const token= req.cookies.token;
+
+    res.clearCookie("token")
+    await blacklistModel.create({token})
+
+    res.status(200).json({
+        message: "User logged out successfully"
+    })
+}
+
+module.exports = { registerUser, loginUser, getMe, logoutUser }
