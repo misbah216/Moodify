62e7e6dbe4cf6922786f9c7ffd18d87a805e81dc
Misbah Saifi <saifimisbah7@gmail.com>
2026-08-27 00:13:03 +0530

    backend completed

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -8,17 +8,20 @@ async function uploadSong(req,res){
 
 
     const tags = id3.read(songBuffer)
-    const songFile = await storageService.uploadFile({
-        buffer : songBuffer,
-        filename : tags.title+ ".mp3",
-        folder : "/Misbah/moodify/songs"
-    })
 
-    const posterFile = await storageService.uploadFile({
-        buffer: tags.image.imageBuffer,
-        filename : tags.title + ".jpeg",
-        folder : "/Misbah/moodify/posters"
-    })
+    
+    const [ songFile, posterFile ] = await Promise.all([
+        storageService.uploadFile({
+            buffer : songBuffer,
+            filename : tags.title+ ".mp3",
+            folder : "/Misbah/moodify/songs"
+        }),
+        storageService.uploadFile({
+            buffer: tags.image.imageBuffer,
+            filename : tags.title + ".jpeg",
+            folder : "/Misbah/moodify/posters"
+        })
+    ])
 
     const song = await songModel.create({
         title :tags.title,
@@ -32,8 +35,20 @@ async function uploadSong(req,res){
         message :"song created successfully",
         song
     })
-
-
 }
 
-module.exports = {uploadSong}
+async function getSong(req,res){
+    const {mood} = req.query
+    const song=await songModel.findOne({
+        mood
+    })
+
+    res.status(200).json({
+        message:"song fetched successfully.",
+        song,
+    })
+}
+
+
+
+module.exports = {uploadSong , getSong}
