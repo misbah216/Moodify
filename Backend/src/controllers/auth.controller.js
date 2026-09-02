5f3235c624ec22ce66a19660d14f73066bce7bbe
Misbah Saifi <saifimisbah7@gmail.com>
2026-09-02 21:22:32 +0530

    fix cross-site cookie settings

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -32,7 +32,12 @@ async function registerUser(req, res) {
         expiresIn: "3d"
     })
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        maxAge: 7*24*60*60*1000
+    });
 
     res.status(201).json({message: "User registered successfully", token});
 }
